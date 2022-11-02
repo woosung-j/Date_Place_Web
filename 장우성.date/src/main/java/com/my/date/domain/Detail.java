58df57ef25b832e95c5ccd37e0014d65e0d9ea63
haks12 <haks12@kakao.com>
2022-11-02 23:56:24 +0900

    domain.detail

---
+++ 장우성.date/src/main/java/com/my/date/domain/Detail.java
@@ -0,0 +1,22 @@
+package com.my.date.domain;
+
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Detail {
+	private int detailId;
+	private int placeId;
+	private String address;
+	private int tel;
+	private int openingHours;
+	private int closingHours;
+	private String dayoff;
+	private String contact;
+	private String parking;
+	private LocalDate createdAt;
+	private LocalDate updatedAt;
+}
