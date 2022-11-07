a3e0ca69b8cd6495d9f8fb2037325548f196b528
haks12 <haks12@kakao.com>
2022-11-07 22:50:25 +0900

    1107 detail list 조회
    우편번호, 법정동 삭제
    수정 후 admin 장소조회로 넘어가기
    오타수정

--- 장우성.date/src/main/java/com/my/date/web/AdminController.java
+++ 장우성.date/src/main/java/com/my/date/web/AdminController.java
@@ -2,28 +2,73 @@ package com.my.date.web;
 
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.my.date.domain.Declaration;
 import com.my.date.domain.Detail;
+import com.my.date.domain.Menu;
+import com.my.date.domain.Place;
+import com.my.date.service.DeclarationService;
 import com.my.date.service.DetailService;
+import com.my.date.service.MenuService;
+import com.my.date.service.PlaceService;
 
 @RestController
 @RequestMapping("admin")
 public class AdminController {
+    @Autowired private DeclarationService declarationService;
+    @Autowired private MenuService menuService;
+    @Autowired private PlaceService placeService;
 	@Autowired private DetailService detailService;
+	
+	@GetMapping("place")
+    public ModelAndView main(ModelAndView mv) {
+        mv.setViewName("place/placelist");
+        return mv;
+    }
+
+    @GetMapping("/placelist")
+    public List<Place> getPlaces() {
+        return placeService.getPlaces();
+    }
+
+    @GetMapping("declare")
+    public ModelAndView declare(ModelAndView mv) {
+        mv.setViewName("admin/declaration/declareList");
+
+        return mv;
+    }
+
+    @GetMapping("declare/list")
+    public List<Declaration> declareList() {
+        return declarationService.getDeclareList();
+    }
+
+    @PatchMapping("declare/toggleconfirm/{declareId}/{confirm}")
+    public int toggleConfirm(@PathVariable int declareId, @PathVariable int confirm) {
+        return declarationService.fixConfirm(declareId, confirm);
+    }
+
+    @GetMapping("menu")
+    public ModelAndView menu(ModelAndView mv) {
+        mv.setViewName("admin/menu/patchmenu");
+        return mv;
+    }
+
+    @GetMapping("menu/getMenus")
+    public List<Menu> getMenus() {
+        return menuService.getMenus();
+    }
 
 	@GetMapping("detail")
 	public ModelAndView detail(ModelAndView mv) {
@@ -31,19 +76,11 @@ public class AdminController {
 		return mv;
 	}
 
-	@GetMapping("detail/getDatails")
+	@GetMapping("detail/getDetails")
 	public List<Detail> getDetails() {
 		return detailService.getDetails();
 	}
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
-//	@GetMapping("/detail")
-//	public ModelAndView selectDetail(ModelAndView mv, @RequestParam("detailId") int detailId, HttpSession session) {
-//		List<Detail> detailList = detailService.selectDetails();
-//		mv.addObject("detailList", detailList);
-//		mv.setViewName("");
-//		return mv;
-//	}
-//	
 	@PostMapping("/add")
 	public void addDetail(@RequestBody Detail detail) {
 		detailService.addDetail(detail);
@@ -51,7 +88,7 @@ public class AdminController {
 	
 	@PutMapping("/fix")
 	public void fixDetail(@RequestBody Detail detail) {
-		detailService.addDetail(detail);
+		detailService.fixDetail(detail);
 	}
 	
 	@DeleteMapping("del/{detailId}")
