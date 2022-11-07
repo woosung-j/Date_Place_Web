79eb2c5ee3c10d9834cd5a190f878d70cbe87be4
haks12 <haks12@kakao.com>
2022-11-07 23:48:13 +0900

    1107 수정

--- 장우성.date/src/main/java/com/my/date/domain/Detail.java
+++ 장우성.date/src/main/java/com/my/date/domain/Detail.java
@@ -4,9 +4,7 @@ import java.time.LocalDate;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
 @NoArgsConstructor
