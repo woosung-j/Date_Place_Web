bf57ad90b0cda57b5f69517975d41eef8ac81e08
mijihime <101626564+mijihime@users.noreply.github.com>
2022-11-08 14:55:12 +0900

    리뷰 추가 기능 구현
    파일 업로드 미구현

--- 장우성.date/src/main/java/com/my/date/web/ReviewController.java
+++ 장우성.date/src/main/java/com/my/date/web/ReviewController.java
@@ -2,11 +2,14 @@ package com.my.date.web;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -37,4 +40,22 @@ public class ReviewController {
 	public double getReviewAvg(@PathVariable int placeId) {
 		return reviewService.getReviewAvg(placeId);
 	}	
+	
+    @GetMapping("add")
+    public ModelAndView add(ModelAndView mv) {
+    	mv.addObject("placeId", 1);
+    	mv.setViewName("review/add");
+        return mv;
+    }
+    
+    @PostMapping("add")
+    public int addReview(HttpServletRequest request, @RequestBody Review review) {
+    	HttpSession session = request.getSession(false);
+        if(session == null || session.getAttribute("userId") == null) {
+            return 0;
+        }
+        int userId = (int) session.getAttribute("userId");
+        review.setUserId(userId);
+        return reviewService.addReview(review);
+    }
 }
