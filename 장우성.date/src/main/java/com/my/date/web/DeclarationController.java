01bad4e9075f6a5faa46be0386912a528bfa891f
mijihime <101626564+mijihime@users.noreply.github.com>
2022-11-13 18:51:20 +0900

    장소 신고 기능 구현

--- 장우성.date/src/main/java/com/my/date/web/DeclarationController.java
+++ 장우성.date/src/main/java/com/my/date/web/DeclarationController.java
@@ -1,15 +1,23 @@
 package com.my.date.web;
 
-import com.my.date.domain.MyDeclarationDto;
-import com.my.date.service.DeclarationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.my.date.domain.Declaration;
+import com.my.date.domain.MyDeclarationDto;
+import com.my.date.service.DeclarationService;
 
 @RestController
 @RequestMapping("declaration")
@@ -29,6 +37,35 @@ public class DeclarationController {
         return mv;
     }
 
+	@GetMapping("/add/{placeId}")
+	public ModelAndView add(@PathVariable int placeId, HttpServletRequest request, ModelAndView mv) {
+		HttpSession session = request.getSession(false);
+		if (session == null || session.getAttribute("userId") == null) {
+			mv.setViewName("redirect:/user/login");
+		} else {
+			mv.addObject("placeId", placeId);
+			mv.setViewName("declaration/add");
+		}
+		return mv;
+	}
+	
+	@GetMapping("/add/getPlaceName/{placeId}")
+	public String getPlaceName(@PathVariable int placeId) {
+		return declarationService.getPlaceName(placeId);
+	}
+	
+	@PostMapping("/add/{placeId}")
+	public int addDeclaration(HttpServletRequest request, @RequestBody Declaration declaration) {
+		HttpSession session = request.getSession(false);
+		if (session == null || session.getAttribute("userId") == null) {
+			return -1;
+		}
+		int userId = (int) session.getAttribute("userId");
+		declaration.setUserId(userId);
+
+		return declarationService.addDeclaration(declaration);
+	}
+	
     @GetMapping("/list")
     public List<MyDeclarationDto> getDeclareList(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
